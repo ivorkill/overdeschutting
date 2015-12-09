ac439f0f902b1fb10c6e54a48c4f4109e9f18a58
ivorkill <davevanrossum@gmail.com>
2015-12-09 15:02:52 +0100

    update

--- schutting/views/songs.php
+++ schutting/views/songs.php
@@ -1,14 +1,14 @@
-<?php
-
-echo '<section>';
-
-while ($song = $result->fetch_assoc()) {
-	
-	echo '<article>';
-	echo '<img src='.$song['img'].'>';
-	echo '<h1>'.$song['id'].' '.$song['artist'].' : '.$song['song'].'</h1>';
-	echo '<content>'.$song['content'].'</content>';
-	echo '</article>';
-}
-
-echo '</section>';
+<div id="articles">
+	<?php
+		echo '<section>';
+		while ($song = $result->fetch_assoc()) {
+		echo '<article>';
+		echo '<iframe src="http://www.youtube.com/embed/'.$song['url'].'"></iframe>';
+		$str =  '<h1>'.$song['id'].' '.$song['artist'].' : '.$song['song'].'</h1>';
+		echo utf8_encode($str);
+		echo '<content>'.$song['content'].'</content>';
+		echo '</article>';
+		}
+		echo '</section>';
+	?>
+</div>
