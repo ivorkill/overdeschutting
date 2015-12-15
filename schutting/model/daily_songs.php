6a731e98081cf8bb4d20e5d9fa77295808fc77e3
Alexstraza <18281@ma-web.nl>
2015-12-15 19:56:18 +0100

    Update daily_songs.php

--- schutting/model/daily_songs.php
+++ schutting/model/daily_songs.php
@@ -1,13 +1,10 @@
 <?php
 	$total_songs = 100;
 	$offset = 5;
-<<<<<<< HEAD
-=======
 	$upper_limit = ($current_day - 2) * $offset;
-    $sql = "SELECT * FROM songs ORDER BY id DESC LIMIT ".$upper_limit. "," . $offset;
->>>>>>> 2a9d3bb36bba21caa981dadd2cba74efc0c53ed0
+	$sql = "SELECT * FROM songs ORDER BY id DESC LIMIT ".$upper_limit. "," . $offset;
 	$upper_limit = $total_songs - $offset * ($current_day - 2);
 	$bottom_limit  = $upper_limit - $offset + 1;
-    $sql = "SELECT * FROM songs WHERE id BETWEEN $bottom_limit AND $upper_limit ORDER BY id DESC";
-    $result = $mysqli->query($sql);
+    	$sql = "SELECT * FROM songs WHERE id BETWEEN $bottom_limit AND $upper_limit ORDER BY id DESC";
+    	$result = $mysqli->query($sql);
 ?>
