cf0db48529b30b4192ea5a9fee4518393a62f50d
ivorkill <davevanrossum@gmail.com>
2015-12-15 20:15:29 +0100

    hi

--- schutting/model/daily_songs.php
+++ schutting/model/daily_songs.php
@@ -1,11 +1,8 @@
 <?php
 	$total_songs = 100;
 	$offset = 5;
-<<<<<<< HEAD
-=======
 	$upper_limit = ($current_day - 2) * $offset;
     $sql = "SELECT * FROM songs ORDER BY id DESC LIMIT ".$upper_limit. "," . $offset;
->>>>>>> 2a9d3bb36bba21caa981dadd2cba74efc0c53ed0
 	$upper_limit = $total_songs - $offset * ($current_day - 2);
 	$bottom_limit  = $upper_limit - $offset + 1;
     $sql = "SELECT * FROM songs WHERE id BETWEEN $bottom_limit AND $upper_limit ORDER BY id DESC";
